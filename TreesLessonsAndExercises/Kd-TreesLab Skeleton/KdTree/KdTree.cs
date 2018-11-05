2fbdcd42ec6edf27c34eb473e2453cd0422b0f9c
nikola <nikolavodeni4arov@gmail.com>
2018-11-05 19:50:34 +0200

    KD tree implementation of insert and contains.

--- TreesLessonsAndExercises/Kd-TreesLab Skeleton/KdTree/KdTree.cs
+++ TreesLessonsAndExercises/Kd-TreesLab Skeleton/KdTree/KdTree.cs
@@ -14,12 +14,73 @@ public class KdTree
 
     public bool Contains(Point2D point)
     {
-        throw new NotImplementedException();
+        return this.SearchRecursively(this.root, point) != null;
+    }
+    private Node SearchRecursively(Node node, Point2D point, int depth = 0)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+
+        var compared = this.Compare(node.Point, point, depth);
+        if (compared > 0)
+        {
+            return this.SearchRecursively(node.Left, point, depth + 1);
+        }
+        else if (compared < 0)
+        {
+            return this.SearchRecursively(node.Right, point, depth + 1);
+        }
+
+        return node;
     }
 
     public void Insert(Point2D point)
     {
-        throw new NotImplementedException();
+        this.root = this.InsertRecursively(this.root, point);
+    }
+    private Node InsertRecursively(Node node, Point2D point, int depth = 0)
+    {
+        if (node == null)
+        {
+            return new Node(point);
+        }
+
+        var compared = this.Compare(node.Point, point, depth);
+        if (compared > 0)
+        {
+            node.Left = this.InsertRecursively(node.Left, point, depth + 1);
+        }
+        else if (compared < 0)
+        {
+            node.Right = this.InsertRecursively(node.Right, point, depth + 1);
+        }
+
+        return node;
+    }
+    private int Compare(Point2D point1, Point2D point2, int depth)
+    {
+        var compared = 0;
+
+        if (depth % 2 == 0)
+        {
+            compared = point1.X.CompareTo(point2.X);
+            if (compared == 0)
+            {
+                compared = point1.Y.CompareTo(point2.Y);
+            }
+        }
+        else
+        {
+            compared = point1.Y.CompareTo(point2.Y);
+            if (compared == 0)
+            {
+                compared = point1.X.CompareTo(point2.X);
+            }
+        }
+
+        return compared;
     }
 
     public void EachInOrder(Action<Point2D> action)
